90d88103d2c54943f43272f698f7b12b65218a62
milekill <andreas_bastian@hotmail.com>
2026-06-16 23:46:46 -0400

    Primer commit de mi proyecto

--- js/index.js
+++ js/index.js
@@ -92,3 +92,20 @@ $(function () {
     $(this).carousel("pause");
     });
 });
+
+
+$(function () {
+
+    $("#btnAlerta").on("click", function () {
+        let tarea = $("#agregartarea").val();
+        $("#caja").append("<li> " + tarea + "</li>");
+        $("#agregartarea").val("");
+    });
+
+
+    $("ul").on("click","li", function () {
+    $(this).remove()
+    console.log("hola")
+    });
+
+});
